852d19cc8d856063dacfc162260e44fdb63f1449
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-28 22:26:59 +0900

    Simplify math

--- functions/humantime.fish
+++ functions/humantime.fish
@@ -1,9 +1,9 @@
 function humantime -a ms -d "Turn milliseconds into a human-readable string"
     set --query ms[1] || return
 
-    set --local hours (math --scale=0 $ms / \(3600 x 1000\))
-    set --local mins (math --scale=0 $ms / \(60 x 1000\) % 60)
-    set --local secs (math --scale=0 $ms / 1000 % 60)
+    set --local secs (math --scale=1 $ms/1000 % 60)
+    set --local mins (math --scale=0 $ms/60000 % 60)
+    set --local hours (math --scale=0 $ms/3600000)
 
     test $hours -gt 0 && set --local --append out $hours"h"
     test $mins -gt 0 && set --local --append out $mins"m"
